07ae61721a6907deb499ef314f2ccbab4fe21f87
Sai Sudha <80373665+SaiSudha123@users.noreply.github.com>
2021-03-19 21:34:10 +0530

    Add files via upload

---
+++ design patterns/Day 1/Session 2/Singleton Handson/src/DBConn.java
@@ -0,0 +1,17 @@
+
+public class DBConn {
+
+	
+	private static DBConn instance;
+	
+	private DBConn() {
+		
+	}
+	
+	public static DBConn getInstance() {
+		if(instance == null)
+			instance = new DBConn();
+		
+		return instance;
+	}
+}
